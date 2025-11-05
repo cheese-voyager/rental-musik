6f85978510b4d385b76ddc82a75340da109d35f6
zeeeeeeeesh <zakhwaaliyamaryam78@gmail.com>
2025-11-06 04:50:54 +0700

    bikin login page dan php serta cssnya, bikin conn.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php 
+    session_start();
+    include 'conn.php';
+ 
+    // menangkap data yang dikirim dari form login
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+ 
+    // menyeleksi data pada tabel admin dengan username dan password yang sesuai
+    $data = mysqli_query($connect, "SELECT * FROM user WHERE email='$email' and password='$password'");
+ 
+    // menghitung jumlah data yang ditemukan
+    $cek = mysqli_num_rows($data);
+ 
+    if($cek > 0){
+        $_SESSION['email'] = $email;
+        $_SESSION['status'] = "login";
+        header("location:tableShow.php");
+    }
+    else{
+        echo "<script> alert ('Login gagal! Email atau password tidak benar');</script>";
+        echo "<script> window.location ='login.html';</script>";
+    }
+?>
